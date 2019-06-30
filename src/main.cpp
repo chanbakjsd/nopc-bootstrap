e9a2bab04f8b93173fe2202bfdb790f542064752
superwhiskers <whiskerdev@protonmail.com>
2019-06-30 04:07:35 -0500

    compiler: make CMakeLists.txt work with llvm, add file for grammar and add formatting rules for clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <llvm/Support/CommandLine.h>
 
-using std::cout;
+using namespace llvm;
 
-int main() {
-  cout << "Hello\n";
-  return 0;
+int main(int argc, char **argv) {
+	cl::ParseCommandLineOptions(argc, argv);
+
+	// TODO: temporary, add argument parsing stuff using llvm's CommandLine.h here
+	//       docs: https://llvm.org/docs/CommandLine.html
+	std::cout << "hello, world!" << std::endl;
+	return 0;
 }
